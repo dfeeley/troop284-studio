354ea806c430447cf25f92028bbb4f4ac1dbd348
David Feeley <davidpfeeley@gmail.com>
2021-09-03 15:48:37 -0700

    Added @sanity/cli to package.json for netlify builds

--- src/dashboardConfig.js
+++ src/dashboardConfig.js
@@ -6,6 +6,12 @@ export default {
         title: 'My Netlify deploys',
         sites: [
           {
+            title: 'Troop284 Studio',
+            apiId: '5ab138f2-ae5d-41b7-ac8d-8d546e69898d',
+            buildHookId: 'https://api.netlify.com/build_hooks/6132a55122f9e2bce03acd9f',
+            name: 'friendly-davinci-871b01',
+          },
+          {
             title: 'Troop 284',
             apiId: '7d37bf15-d54e-44fe-9521-31b301228f76',
             buildHookId: 'https://api.netlify.com/build_hooks/613139cb40e8e9241ded6dc7',
