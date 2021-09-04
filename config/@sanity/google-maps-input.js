c18b926f416037fca7e8646399faa55a857628ae
David Feeley <davidpfeeley@gmail.com>
2021-09-03 20:36:58 -0700

    Move google maps key to .env

---
+++ config/@sanity/google-maps-input.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: process.env.NODE_ENV_GOOGLE_MAPS_API_KEY,
+  defaultZoom: 11,
+  defaultLocale: null,
+  defaultLocation: {
+    lat: 40.7058254,
+    lng: -74.1180863
+  }
+}
