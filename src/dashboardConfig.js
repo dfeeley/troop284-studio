2e3471b6b386ec208bad07d378db208ddcc3d6ea
David Feeley <davidpfeeley@gmail.com>
2021-09-08 11:02:52 -0700

    Fixed build hook urls in dashboard config

--- src/dashboardConfig.js
+++ src/dashboardConfig.js
@@ -8,13 +8,13 @@ export default {
           {
             title: 'Troop284 Studio',
             apiId: '5ab138f2-ae5d-41b7-ac8d-8d546e69898d',
-            buildHookId: 'https://api.netlify.com/build_hooks/6132a55122f9e2bce03acd9f',
+            buildHookId: '6132a55122f9e2bce03acd9f',
             name: 'friendly-davinci-871b01',
           },
           {
             title: 'Troop 284',
             apiId: '7d37bf15-d54e-44fe-9521-31b301228f76',
-            buildHookId: 'https://api.netlify.com/build_hooks/613139cb40e8e9241ded6dc7',
+            buildHookId: '613139cb40e8e9241ded6dc7',
             name: 'dazzling-euler-a04f2a',
           }
         ]
